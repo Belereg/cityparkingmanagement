b5c535cea172b1c6d7e36d386b5d38bcf509eab2
Alexandru Nastase <nalexandru27@yahoo.com>
2022-02-09 22:15:27 +0200

    base project with dependencies and db connection

---
+++ src/main/java/com/example/cityparkingmanagement/CityparkingmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.cityparkingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityparkingmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityparkingmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cityparkingmanagement/CityparkingmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cityparkingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityparkingmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
